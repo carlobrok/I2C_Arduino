5c6e817a47f30cf1389ac2351a9d9c28187658bc
carlobrok <carlo.brokering@outlook.de>
2019-06-15 17:52:49 +0200

    Test I2C recieve

--- I2C_motoren_arduino/I2C_motoren_arduino.ino
+++ I2C_motoren_arduino/I2C_motoren_arduino.ino
@@ -1,9 +1,41 @@
-void setup() {
-  // put your setup code here, to run once:
+#define SERIAL_OUTPUT
+#include <Wire.h>
+//#include "KamelMotor.h"
 
+void setup() {
+  Wire.begin(0x08);
+  Wire.onReceive(receiveEvent);
+
+#ifdef SERIAL_OUTPUT
+  Serial.begin(115200);
+#endif
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  // Motoren ansteuern
+
+  //delay(100);
+}
+
+void receiveEvent(int byte_amount) {
+  /*if (byte_amount != 2) {
+#ifdef SERIAL_OUTPUT
+    Serial.print("Wrong byte amount! ");
+    Serial.println(byte_amount);
+#endif
+  } else {
+*/
+    Serial.print("Anzahl: ");
+    Serial.println(byte_amount);
+    while (Wire.available()) { // loop through all but the last
+      int c = Wire.read(); // receive byte as a character
+      Serial.print(c);         // print the character
+      Serial.print(" ");
+    }
+    Serial.println();
+    
+    // Update motor values
+
+  //}
 }
