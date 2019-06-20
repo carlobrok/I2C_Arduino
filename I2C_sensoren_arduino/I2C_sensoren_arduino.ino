8d473f8be3a9214e92cdb716a97487c4f41a722f
Jam4l <jamal.salif.asaana@gmail.com>
2019-06-20 14:47:59 +0200

    Added test code

--- I2C_sensoren_arduino/I2C_sensoren_arduino.ino
+++ I2C_sensoren_arduino/I2C_sensoren_arduino.ino
@@ -1,9 +1,34 @@
+
+#include <Wire.h>
+#define SLAVE_ADRESS 0x08
+
+
+byte response[2]; // this data is sent to PI
+volatile short LDR_value; // Global Declaration
+const int LDR_pin=A0; //pin to which LDR is connected A0 is analog 
+
 void setup() {
-  // put your setup code here, to run once:
+  
+  Wire.begin(SLAVE_ADDRESS); // this will begin I2C Connection with 0x40 address
+  Wire.onRequest(sendData); // sendData is funtion called when Pi requests data
+  pinMode(LDR_pin,INPUT);
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+  LDR_value= 500 ;
+  // Arduino returns 10bit data but we need to convert it to 8bit 
+  LDR_value=map(LDR_value,0,1023,0,255);
+  response[0]=(byte)LDR_value;
+  Wire.write(response,2); // return data to PI
 
+  
+  delay(1000);
 }
+
+byte data = 0;
+
+/*void sendData()
+{
+}*/
