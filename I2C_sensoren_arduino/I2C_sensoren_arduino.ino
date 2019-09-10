eebfae18ee9d78f692660f474b6b64a85adabc9e
carlobrok <carlo.brokering@outlook.de>
2019-09-10 12:04:02 +0200

    requestEvent method added

--- I2C_sensoren_arduino/I2C_sensoren_arduino.ino
+++ I2C_sensoren_arduino/I2C_sensoren_arduino.ino
@@ -1,13 +1,20 @@
 #include <Wire.h>
 
 #define SLAVE_ADDRESS 0x08
-
 #define SERIAL_OUTPUT
+
+#define ALL_SENSOR_VALUES 1
+#define DIGITAL_SENSOR_VALUES 2
+#define ANALOG_SENSOR_VALUES 3
+
+
+int command = 0;
 
 void setup() {
   Wire.begin(SLAVE_ADDRESS);
   Wire.onReceive(receiveEvent);
-
+  Wire.onRequest(requestEvent);
+  
   pinMode(LED_BUILTIN, OUTPUT);
 
 #ifdef SERIAL_OUTPUT
@@ -18,8 +25,6 @@ void setup() {
 
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
 }
 
 
@@ -27,18 +32,40 @@ void receiveEvent(int byte_amount) {
 
 #ifdef SERIAL_OUTPUT
   Serial.print("Anzahl: ");
-  Serial.println(byte_amount);
+  Serial.print(byte_amount);
 #endif
 
   digitalWrite(LED_BUILTIN, HIGH);
 
-  int command = Wire.read();
+  command = Wire.read();
+#ifdef SERIAL_OUTPUT
+  Serial.print(command);
   
-  if(command == 1) {
-    Serial.println("1");
+  if(command == ALL_SENSOR_VALUES) {
+    Serial.print(" write all sensor values");
+  } else if(command == DIGITAL_SENSOR_VALUES) {
+    Serial.print(" write all sensor values");
+  } else if(command == ANALOG_SENSOR_VALUES) {
+    Serial.print(" write all sensor values");
   }
+  Serial.println();
+#endif
   
-
-
   digitalWrite(LED_BUILTIN, LOW);
+}
+
+
+void requestEvent() {
+  if(command == ALL_SENSOR_VALUES) {
+    Wire.write(5); // respond with message of 6 bytes
+    Wire.write(5); // respond with message of 6 bytes
+    Wire.write(5); // respond with message of 6 bytes
+    Wire.write(5); // respond with message of 6 bytes
+    Wire.write(5); // respond with message of 6 bytes
+    Wire.write(5); // respond with message of 6 bytes
+  } else if(command == DIGITAL_SENSOR_VALUES) {
+    Serial.print(" write all sensor values");
+  } else if(command == ANALOG_SENSOR_VALUES) {
+    Serial.print(" write all sensor values");
+  }
 }
